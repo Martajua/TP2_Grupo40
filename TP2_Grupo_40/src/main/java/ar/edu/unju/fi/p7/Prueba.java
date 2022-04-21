6b9a62a72ada3c3a0ecf79ef899f48079c5d984c
juaco <juaco@LAPTOP-23S2M07S>
2022-04-21 11:25:34 -0300

    Actualizacion punto7

--- TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/Prueba.java
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/Prueba.java
@@ -1,25 +1,28 @@
 package ar.edu.unju.fi.p7;
 
+//import java.util.Scanner;
 //import java.util.*;
 public class Prueba {
 
-	    public static void main(String[] args){
-
-	    int i,j;
-	 
-	    j=40;      
-
-
-	    for (i=2; i<0; i++){
-	    	i=j+i-1;
-	    	if (i % 2 != 0);
-	    		//if (j > 0);
-	    			j=j+i;
-	    			
-	    	System.out.println(i+"i");
-	      System.out.println(j+"j");
-	    }
-	    
-	    //System.out.print("la suma de la serie es = :  "+j);
-	}
-	}
+	
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in)
+        	//int j=40;
+        	int i;
+        for (i = 2; i <0 ; i++) {    //inicio del for
+        
+        	//j=0;
+        	i=40-i;
+        
+        //    j=j+i-1;
+            
+            System.out.println(i + "i");
+            //System.out.println(j + "j");
+            //i=i-1;
+        }  //fin del for
+        
+        //System.out.print sin ln los datos salen uno al lado del otro.
+        //System.out.println("\nFin programa");                                                                     
+    }
+}
+	
