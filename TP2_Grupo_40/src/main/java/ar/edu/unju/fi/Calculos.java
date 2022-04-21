281b87b7fb76067c6533c4123c0195f353575a36
Agustin <103860271+AgusTactaca@users.noreply.github.com>
2022-04-20 23:16:22 -0300

    Ejercicio 16

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/Calculos.java
@@ -0,0 +1,38 @@
+package fi.unju.edu.ar.ejercicio16;
+
+
+public class Calculos {
+	public int n;
+	
+	public double CalcularSumatoria(int n) {
+		double resultado = 0;
+		this.n = n;
+		int k = n;
+		for (int i = 0; i < n; i++) {
+			resultado = resultado + Math.pow((k*(k+1)/2),2);
+			k = k-1;  
+		}
+		return resultado;
+	}
+	
+	public double CalcularProductoria(int n) {
+		double resultado = 1;
+		this.n = n;
+		int k = n;
+		for (int i = 0; i < n; i++) {
+			resultado = resultado * (k*(k+4));	
+			k = k-1;
+		}
+		return resultado;
+	}
+	
+	public int getN() {
+		return n;
+	}
+
+
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+}
