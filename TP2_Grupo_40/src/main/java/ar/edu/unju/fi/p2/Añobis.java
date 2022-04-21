4d65cfa19688a0d7b682892ab94043ec4368a71c
juaco <juaco@LAPTOP-23S2M07S>
2022-04-20 23:58:55 -0300

    Punto 2

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p2/Añobis.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.p2;
+import java.util.Scanner;
+public class EsBisiesto {
+	@SuppressWarnings("resource")
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese el año a consultar: ");
+		int año=sc.nextInt();
+		
+	//	para que un año sea bisiesto debe ser divisible por 4  por 400	
+		if((año % 4 == 0) && (año % 100 != 0) || (año % 400 == 0)) {
+			System.out.println("El año ingresado es Bisiesto.");
+		}
+		else {
+			System.out.println("El año ingresado NO es un año Bisiesto.");
+		}
+	}
+}
