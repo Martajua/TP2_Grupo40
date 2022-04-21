582a33ef4aec85397e4af432e5a948ad8f0412f1
juaco <juaco@LAPTOP-23S2M07S>
2022-04-21 01:33:14 -0300

    Punto 16

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p16/CalculoMatematico.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.p16;
+
+import java.util.Scanner;
+
+import ar.edu.unju.fi.Calculos;
+
+public class CalculoMatematico {
+	
+	public int n;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduzca las veces de repeticiones n: "); int n = scanner.nextInt();
+		Calculos Sumatoria = new Calculos();
+		double resultadosum = Sumatoria.CalcularSumatoria(n);
+		System.out.println("El resultado de la sumatoria es: " + resultadosum);
+		Calculos Productoria = new Calculos();
+		double resultadopro = Productoria.CalcularProductoria(n);
+		System.out.println("El resultado de la productoria es: " + resultadopro);
+	}
+	
+}
