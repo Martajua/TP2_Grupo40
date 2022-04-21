ae629527f62f20400c7cee00e338f47b8d81de64
juaco <juaco@LAPTOP-23S2M07S>
2022-04-21 01:29:41 -0300

    Principal Ejercicio 15

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p15/PrincipalEjercicio15.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.p15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+@SpringBootApplication
+public class PrincipalEjercicio15 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipalEjercicio15.class, args);
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Dia nacimiento: "); int dia = scanner.nextInt();
+		System.out.println("Mes nacimiento: "); int mes = scanner.nextInt();
+		System.out.println("Anio nacimiento: "); int anio = scanner.nextInt();
+		Ejercicio15 CalculoEdad = new Ejercicio15();
+		int edad = CalculoEdad.CalcularEdad(dia, mes, anio);
+		System.out.println("Tu edad es: " + edad + " anios");
+		Ejercicio15 SignoZodiaco = new Ejercicio15();
+		String zodiaco = SignoZodiaco.CalcularSignoZodiaco(dia, mes);
+		System.out.println("Tu signo es: " + zodiaco);
+		Ejercicio15 Estacion = new Ejercicio15();
+		String estacion = Estacion.CalcularEstacion(dia, mes);
+		System.out.println("Estacion del anio segun tu fecha de nacimiento: " + estacion);
+	}
+}
