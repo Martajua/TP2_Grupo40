7ef32d6e03b6fdc1fc10259b114bd44da9179cbf
juaco <juaco@LAPTOP-23S2M07S>
2022-04-20 23:47:43 -0300

    Punto 8

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p8/Punto8.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.p8;
+
+public class Punto8 {
+
+	public static void main(String[] args) {
+	    int i = 0;
+	    
+	    do {
+	      System.out.println(i);
+	      i+=4;
+	    } while(i <= 100);
+
+	}
+
+}
+
