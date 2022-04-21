c0c776979cfac35eb7c3ae33ec45db94ff719aa8
juaco <juaco@LAPTOP-23S2M07S>
2022-04-20 23:49:13 -0300

    Punto11

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p11/Punto11DoWhile.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.p11;
+
+public class Punto11DoWhile {
+
+	public static void main(String[] args) {
+	    int i = 320;
+	    
+	    do {
+	      System.out.println(i);
+	      i-=20;
+	    } while(i >= 160);
+	}
+}
+
+
