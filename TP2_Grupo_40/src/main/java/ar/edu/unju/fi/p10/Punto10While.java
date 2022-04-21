7452dc809433337a6e7a740075aeef20332c3aef
juaco <juaco@LAPTOP-23S2M07S>
2022-04-20 23:19:23 -0300

    Punto 10

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p10/Punto10While.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.p10;
+
+public class Punto10While {
+
+	public static void main(String[] args) {
+	    int i = 320;
+	    
+	    while(i >= 160) {
+	        System.out.println(i);
+	        i-=20;
+	    }
+	}
+}
+
