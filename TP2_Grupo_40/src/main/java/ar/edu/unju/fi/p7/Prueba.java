ab965e1ad88f7e6ab1eba1a5ce04f2a8b9eab038
juaco <juaco@LAPTOP-23S2M07S>
2022-04-21 10:41:23 -0300

    Pruba punto7

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/Prueba.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.p7;
+
+import java.util.*;
+public class Prueba {
+
+	    public static void main(String[] args){
+
+	    int i,j;
+	 
+	    j=40;      
+
+
+	    for (i=2; i<0; i++){
+	    	i=j+i-1;
+	    	if (i % 2 != 0);
+	    		//if (j > 0);
+	    			j=j+i;
+	    			
+	    	System.out.println(i+"i");
+	      System.out.println(j+"j");
+	    }
+	    
+	    //System.out.print("la suma de la serie es = :  "+j);
+	}
+	}
--- TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/SecuenciaNumFor.java
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/SecuenciaNumFor.java
@@ -1,32 +1,21 @@
-package ar.edu.unju.fi.p7;
 
-public class SecuenciaNumFor {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
 package ar.edu.unju.fi.aplicacion.p7;
 
 import java.util.Scanner;
-public class Punto7 {
+public class Prueba {
 	
 	public static void main(String[] args) {
 
 	Scanner sc = new Scanner(System.in);
        int j=40;
-       int k=0;
-        for (int i=2; i<=j; i++) {
+        for (int i=2; i<0; i++) {
         	i=i++;
-        	k=j+i-1;	
-        	
+        	j=j+i-1;	
         if (i % 2 != 0)  {
         		//System.out.println("impar"+ i);
         		//System.out.println(k);
         		
-        	 k=k+i;
+        	 j=j+i;
         		System.out.println(k);
         	} 
         	else {
