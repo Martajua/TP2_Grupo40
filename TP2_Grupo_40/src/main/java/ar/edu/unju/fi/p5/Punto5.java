ad4bf689ecf9e4e0a4c8162a9b611d52d7ce736c
Carla Ramirez <98058616+Car289@users.noreply.github.com>
2022-04-22 22:14:28 -0300

    Update Punto5.java
    Desarrollo del punto 5

--- TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p5/Punto5.java
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p5/Punto5.java
@@ -4,7 +4,50 @@ public class Punto5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese un número: ");
+		int num =sc.nextInt();
+		switch(num) {
+			case 1: 
+				System.out.println("Enero");
+				break;
+			case 2: 
+				System.out.println("Febrero");
+				break;
+			case 3: 
+				System.out.println("Marzo");
+				break;
+			case 4: 
+				System.out.println("Abril");
+				break;
+			case 5: 
+				System.out.println("Mayo");
+				break;
+			case 6: 
+				System.out.println("Junio");
+				break;
+			case 7: 
+				System.out.println("Julio");
+				break;
+			case 8: 
+				System.out.println("Agosto");
+				break;
+			case 9: 
+				System.out.println("Septiembre");
+				break;
+			case 10: 
+				System.out.println("Octubre");
+				break;
+			case 11: 
+				System.out.println("Noviembre");
+				break;
+			case 12: 
+				System.out.println("Diciembre");
+				break;
+			default:
+				System.out.println("El numero ingresado no corresponde a un mes del año");
+			}
+		
 	}
 
 }
