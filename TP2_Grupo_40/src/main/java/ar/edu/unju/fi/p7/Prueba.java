890dfa2e1844379e98d710fb387ecdc84e082219
juaco <juaco@LAPTOP-23S2M07S>
2022-04-22 21:13:24 -0300

    punto7

--- TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/Prueba.java
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p7/Prueba.java
@@ -7,22 +7,14 @@ public class Prueba {
 	
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in)
-        	//int j=40;
+        	int j=40;
         	int i;
-        for (i = 2; i <0 ; i++) {    //inicio del for
-        
-        	//j=0;
-        	i=40-i;
-        
-        //    j=j+i-1;
-            
-            System.out.println(i + "i");
-            //System.out.println(j + "j");
-            //i=i-1;
+        for (i = 2; i < j ;) {    //inicio del for
+        	j=j-i++;
+	            System.out.println(i + "i");
+	            System.out.println(j + "j");
         }  //fin del for
-        
-        //System.out.print sin ln los datos salen uno al lado del otro.
-        //System.out.println("\nFin programa");                                                                     
+                                                                      
     }
 }
 	
