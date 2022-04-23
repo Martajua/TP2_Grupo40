91e10adda6ed00c3cef5d65fbe6afe7f8d190460
juaco <juaco@LAPTOP-23S2M07S>
2022-04-22 21:29:35 -0300

    Actualizacion

---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p17/Punto17.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.p17;
+
+public class Punto17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TP2_Grupo_40/src/main/java/ar/edu/unju/fi/p4/Punto4.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.p4;
+
+public class Punto4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
